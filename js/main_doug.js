2e4907d7bb811250d4e20fda40e66014f5ebb4a2
s22h076-dev <s22h076@ge.osaka-sandai.ac.jp>
2025-10-30 18:27:22 +0900

    Create main_doug.js

---
+++ js/main_doug.js
@@ -0,0 +1,108 @@
+$(function(){
+  /*-------------------------------
+  ハンバーガーメニュー
+  ---------------------------------*/
+  $(".hamburger").click(function () {
+    $(this).toggleClass("active");
+    $("#header .navi").toggleClass("active");
+    $("#header .mask").toggleClass("active");
+  });
+  
+  $(".navi a").click(function () {
+    $(".hamburger").removeClass("active");
+    $("#header .navi").removeClass("active");
+    $("#header .mask").removeClass("active");
+  });
+  
+  $(".mask").click(function () {
+    $(".hamburger").removeClass("active");
+    $("#header .navi").removeClass("active");
+    $("#header .mask").removeClass("active");
+  });
+  
+  /*-------------------------------
+  ドロップダウンメニュー
+  ---------------------------------*/
+  $(".navi .menu .menu-first span").click(function () {
+    $(this).toggleClass("active");
+    $(this).next().slideToggle();
+  });
+  
+  $(".navi .menu .menu-second").click(function () {
+    $(this).prev().toggleClass("active");
+    $(this).slideToggle();
+  });
+
+  /*-------------------------------
+  Inview
+  ---------------------------------*/
+  $(".fadein").on("inview", function () {
+    $(this).addClass("inview");
+  });
+
+  /*-------------------------------
+  タブ切り替え
+  ---------------------------------*/
+/*-------------------------------
+タブ切り替え
+---------------------------------*/
+$(".tab-list .tab-all").addClass("active");
+$(".products-list.all").addClass("active");
+
+$(".tab-list li").click(function () {
+  // まず全てのアクティブ状態をリセット
+  $(".tab-list li").removeClass("active");
+  $(".products-list").removeClass("active");
+
+  // 押されたタブをアクティブ化
+  $(this).addClass("active");
+
+  // 押されたタブに応じて表示切り替え
+  if ($(this).hasClass("tab-all")) {
+    $(".products-list.all").addClass("active");
+  } else if ($(this).hasClass("tab-sofa")) {
+    $(".products-list.sofa").addClass("active");
+  } else if ($(this).hasClass("tab-desk")) {
+    $(".products-list.desk").addClass("active");
+  } else if ($(this).hasClass("tab-chair")) {
+    $(".products-list.chair").addClass("active");
+  } else if ($(this).hasClass("tab-mizusasi")) {
+    $(".products-list.mizusasi").addClass("active");
+  }
+});
+
+
+  /*-------------------------------
+  モーダルウィンドウ
+  ---------------------------------*/
+  // オープン
+  $(".work1 .modal-open").click(function () {
+    $("body").css("overflow-y", "hidden");
+    $(".work1 .modal-container").addClass("active");
+  });
+  
+  $(".work2 .modal-open").click(function () {
+    $("body").css("overflow-y", "hidden");
+    $(".work2 .modal-container").addClass("active");
+  });
+  
+  $(".work3 .modal-open").click(function () {
+    $("body").css("overflow-y", "hidden");
+    $(".work3 .modal-container").addClass("active");
+  });
+
+  // クローズ
+  $(".modal-close").click(function () {
+    $("body").css("overflow-y", "auto");
+    $(".modal-container").removeClass("active");
+  });
+
+  /*-------------------------------
+  アコーディオン
+  ---------------------------------*/
+  $(".faq-list dd").hide();
+  $(".faq-list dt").click(function () {
+    $(this).next().slideToggle();
+    $(this).toggleClass("active");
+  });
+});
