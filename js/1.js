5c78d5a747378ab6a3f5bd1f15db3f685f9d1dfd
yuok10 <s22h018@ge.osaka-sandai.ac.jp>
2025-10-30 16:38:58 +0900

    Add files via upload

--- js/1.js
+++ js/1.js
@@ -3,7 +3,7 @@ const progressBtn = document.getElementById("progress-btn");
 const settingsBtn = document.getElementById("settings-btn");
 
 // レベル①〜⑩を生成
-const totalLevels = 10;
+const totalLevels = 5;
 for (let i = 1; i <= totalLevels; i++) {
   // 2つごとに行を作る
   if (i % 2 === 1) {
@@ -25,12 +25,4 @@ for (let i = 1; i <= totalLevels; i++) {
   row.appendChild(level);
 }
 
-// 進歩ボタン
-progressBtn.addEventListener("click", () => {
-  alert("進歩画面は今後追加されます。");
-});
 
-// 設定ボタン
-settingsBtn.addEventListener("click", () => {
-  alert("設定画面は準備中です。");
-});
